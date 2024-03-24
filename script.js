277fa2b82781666342a1188b32d6c5446e4da73f
TechMark Soluções & Tecnologia <115334886+TechMarq@users.noreply.github.com>
2024-03-23 22:30:33 -0300

    Cartao Fidelidade V2
    Polimento JavaScript

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ function carimbar() {
   for (let i = 0; i < campos.length; i++) {
     if (!campos[i].classList.contains('carimbado') && senhaInput === 'padrao123') {
       campos[i].classList.add('carimbado');
-      campos[i].textContent = "DS"; // Adiciona texto indicando que foi carimbado
+      const img = document.createElement('img');
+      img.src = 'img/logo_DS.png'; // Insira o caminho para a sua imagem aqui
+      img.alt = 'Logo'; // Insira uma descrição alternativa para acessibilidade
+      campos[i].innerHTML = ''; // Limpa o conteúdo anterior
+      campos[i].appendChild(img); // Adiciona a imagem ao campo
       break; // Para depois de carimbar o primeiro campo vazio
     }
   }
